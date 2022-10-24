b148b2f93a878b38a669c496d35cad7cddb49b4a
surendra9111 <108533205+surendra9111@users.noreply.github.com>
2022-10-25 03:31:56 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,115 @@
+resource "azurerm_resource_group" "example" {
+  name     = var.RGName
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = var.Vnetname
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = var.subnetname
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
+resource "azurerm_route_table" "example" {
+  name                = var.RTname
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  route {
+    name                   = "example"
+    address_prefix         = "10.100.0.0/14"
+    next_hop_type          = "VirtualAppliance"
+    next_hop_in_ip_address = "10.10.1.1"
+  }
+}
+
+resource "azurerm_subnet_route_table_association" "example" {
+  subnet_id      = azurerm_subnet.example.id
+  route_table_id = azurerm_route_table.example.id
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = var.nsgname
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = var.nicname
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.0.3.7"
+    primary                       = true
+  }
+  
+}
+
+ resource "azurerm_virtual_machine" "example" {
+   name                  = var.vmname
+   location              = azurerm_resource_group.example.location
+   resource_group_name   = azurerm_resource_group.example.name
+   network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+   vm_size               = "Standard_F2"
+
+  
+   storage_image_reference {
+     publisher = "Canonical"
+     offer     = "UbuntuServer"
+     sku       = "16.04-LTS"
+     version   = "latest"
+   }
+
+   storage_os_disk {
+     name              = "surendra98354"
+     caching           = "ReadWrite"
+     create_option     = "FromImage"
+     managed_disk_type = "Standard_LRS"
+   }
+
+   
+   os_profile {
+     computer_name  = "hostname"
+     admin_username = "testadmin"
+     admin_password = "Password1234!"
+   }
+
+   os_profile_linux_config {
+     disable_password_authentication = false
+   }
+
+   tags = {
+     environment = "staging"
+   }
+ }
+ 
