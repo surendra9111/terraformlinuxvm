dba4e127099de32f89fada6106e5efb2bfd1b38f
surendra9111 <108533205+surendra9111@users.noreply.github.com>
2022-10-25 03:39:02 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,8 +9,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id   = "2da48e21-7c15-4a47-8398-e05f5c1f53cc"
-  tenant_id         = "7fe36efb-60bb-44a8-ab6a-f1348cc0c441"
-  client_id         = "502761a8-4507-4482-b1b8-42526a573e4d"
-  client_secret     = "EH_8Q~-2tsrAEeLY4sRa-TsQq2szb_wt9AqWlbJi"
 }
